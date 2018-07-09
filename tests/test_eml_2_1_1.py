7538ce010ed46e3f1d97aafa956a54a541818dd1
servilla <mark.servilla@gmail.com>
2018-07-09 14:51:50 -0600

    Add new tests for accessRule and additionalMetadataRule

--- tests/test_eml_2_1_1.py
+++ tests/test_eml_2_1_1.py
@@ -29,54 +29,60 @@ logger = daiquiri.getLogger('test_eml_2_1_1: ' + __name__)
 class TestEml_2_1_1(unittest.TestCase):
 
     def setUp(self):
-        eml = Node(names.EML)
-        eml.add_attribute('packageId', 'edi.23.1')
-        eml.add_attribute('system', 'metapype')
+        self.eml = Node(names.EML)
+        self.eml.add_attribute('packageId', 'edi.23.1')
+        self.eml.add_attribute('system', 'metapype')
 
-        access = Node(names.ACCESS, parent=eml)
-        access.add_attribute('authSystem', 'pasta')
-        access.add_attribute('order', 'allowFirst')
-        eml.add_child(access)
+        self.access = Node(names.ACCESS, parent=self.eml)
+        self.access.add_attribute('authSystem', 'pasta')
+        self.access.add_attribute('order', 'allowFirst')
+        self.eml.add_child(self.access)
 
-        allow = Node(names.ALLOW, parent=access)
-        access.add_child(allow)
+        self.allow = Node(names.ALLOW, parent=self.access)
+        self.access.add_child(self.allow)
 
-        principal = Node(names.PRINCIPAL, parent=allow)
-        principal.content = 'uid=gaucho,o=EDI,dc=edirepository,dc=org'
-        allow.add_child(principal)
+        self.principal = Node(names.PRINCIPAL, parent=self.allow)
+        self.principal.content = 'uid=gaucho,o=EDI,dc=edirepository,dc=org'
+        self.allow.add_child(self.principal)
 
-        permission = Node(names.PERMISSION, parent=allow)
-        permission.content = 'all'
-        allow.add_child(permission)
+        self.permission = Node(names.PERMISSION, parent=self.allow)
+        self.permission.content = 'all'
+        self.allow.add_child(self.permission)
 
-        dataset = Node(names.DATASET, parent=eml)
-        eml.add_child(dataset)
+        self.dataset = Node(names.DATASET, parent=self.eml)
+        self.eml.add_child(self.dataset)
 
-        title = Node(names.TITLE, parent=dataset)
-        title.content = 'Green sea turtle counts: Tortuga Island 20017'
-        dataset.add_child(title)
+        self.title = Node(names.TITLE, parent=self.dataset)
+        self.title.content = 'Green sea turtle counts: Tortuga Island 20017'
+        self.dataset.add_child(self.title)
 
-        creator = Node(names.CREATOR, parent=dataset)
-        dataset.add_child(creator)
+        self.creator = Node(names.CREATOR, parent=self.dataset)
+        self.dataset.add_child(self.creator)
 
-        individualName_creator = Node(names.INDIVIDUALNAME, parent=creator)
-        creator.add_child(individualName_creator)
+        self.individualName_creator = Node(names.INDIVIDUALNAME, parent=self.creator)
+        self.creator.add_child(self.individualName_creator)
 
-        surName_creator = Node(names.SURNAME, parent=individualName_creator)
-        surName_creator.content = 'Gaucho'
-        individualName_creator.add_child(surName_creator)
+        self.surName_creator = Node(names.SURNAME, parent=self.individualName_creator)
+        self.surName_creator.content = 'Gaucho'
+        self.individualName_creator.add_child(self.surName_creator)
 
-        contact = Node(names.CONTACT, parent=dataset)
-        dataset.add_child(contact)
+        self.contact = Node(names.CONTACT, parent=self.dataset)
+        self.dataset.add_child(self.contact)
 
-        individualName_contact = Node(names.INDIVIDUALNAME, parent=contact)
-        contact.add_child(individualName_contact)
+        self.individualName_contact = Node(names.INDIVIDUALNAME, parent=self.contact)
+        self.contact.add_child(self.individualName_contact)
 
-        surName_contact = Node(names.SURNAME, parent=individualName_contact)
-        surName_contact.content = 'Gaucho'
-        individualName_contact.add_child(surName_contact)
+        self.surName_contact = Node(names.SURNAME, parent=self.individualName_contact)
+        self.surName_contact.content = 'Gaucho'
+        self.individualName_contact.add_child(self.surName_contact)
 
-        self.node = eml
+        self.additional_metadata = Node(names.ADDITIONALMETADATA, parent=self.eml)
+        self.eml.add_child(self.additional_metadata)
+        self.metadata = Node(names.METADATA, parent=self.additional_metadata)
+        self.metadata.content = '<tag>TAG</tag>'
+        self.additional_metadata.add_child(self.metadata)
+
+        self.node = self.eml
 
 
     def tearDown(self):
@@ -86,8 +92,17 @@ class TestEml_2_1_1(unittest.TestCase):
     def test_validate_node(self):
         self.assertIsNone(validate.node(self.node))
 
+
     def test_validate_tree(self):
         self.assertIsNone(validate.tree(self.node))
+
+
+    def test_accessRule(self):
+        self.assertIsNone(validate.accessRule.validate_rule(self.access))
+
+
+    def test_additionalMetadataRule(self):
+        self.assertIsNone(validate.additionalMetadataRule.validate_rule(self.additional_metadata))
 
 
 def main():
