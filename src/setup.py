f9a57f0b1663ba09d9caa8792798eb29ca0cccdb
Mark Servilla <mark.servilla@gmail.com>
2020-05-07 16:55:11 -0600

    Update installation setup

--- src/setup.py
+++ src/setup.py
@@ -13,19 +13,36 @@
 :Created:
     6/15/18
 """
-import setuptools
+from os import path
+from setuptools import find_packages, setup
 
-setuptools.setup(name='metapype',
-                 version='2019.2.15',
-                 description='Metapype for EML',
-                 author='EDI',
-                 url='https://github.com/PASTAplus/metapype-eml',
-                 license='Apache License, Version 2.0',
-                 packages=setuptools.find_packages(),
-                 include_package_data=True,
-                 exclude_package_data={
-                     '': ['settings.py, properties.py, config.py'],
-                 }, )
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
+    full_license = f.read()
+
+
+setup(
+    name='metapype',
+    version='2020.05.07',
+    description='Metapype for EML', long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='EDI',
+    url='https://github.com/PASTAplus/metapype-eml',
+    license=full_license,
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    exclude_package_data={"": ["settings.py, properties.py, config.py"],},
+    package_dir={"": "src"},
+    python_requires=">3.8.*",
+    entry_points={"console_scripts": ["rendere=rendere.render:main"]},
+    classifiers=['License :: OSI Approved :: Apache Software License', ],
+    # install_requires=SEE environment.yml or requirements.txt
+)
 
 
 def main():
