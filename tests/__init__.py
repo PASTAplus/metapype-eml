daafca215f4c829e9273721661c3af0dc94367d9
Mark Servilla <mark.servilla@gmail.com>
2021-01-30 16:08:27 -0700

    Add logging for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,7 +14,18 @@
 :Created:
     5/7/20
 """
+import logging
 import os
 import sys
+
+import daiquiri
+
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + "/tests.log"
+daiquiri.setup(level=logging.DEBUG,
+               outputs=(daiquiri.output.File(logfile), "stdout",))
+logger = daiquiri.getLogger(__name__)
+
 
 sys.path.insert(0, os.path.abspath("../src"))
