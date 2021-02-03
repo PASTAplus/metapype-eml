63119fea89a033fdcaca0840769ca82fc015bcbc
Mark Servilla <mark.servilla@gmail.com>
2021-02-02 21:09:52 -0700

    Add full support for importing and exporting from generalized XML

--- src/metapype/model/metapype_io.py
+++ src/metapype/model/metapype_io.py
@@ -17,6 +17,7 @@ import json
 
 import daiquiri
 from lxml import etree
+from xml.sax.saxutils import escape
 
 from metapype.model.node import Node
 
@@ -42,20 +43,85 @@ def _from_dict(node: dict, parent: Node = None) -> Node:
     if parent is not None:
         node.parent = parent
 
-    attributes = body[1]["attributes"]
+    nsmap = body[1]["nsmap"]
+    if nsmap is not None:
+        for nsp in nsmap:
+            node.add_namespace(nsp, nsmap[nsp])
+
+    prefix = body[2]["prefix"]
+    if prefix is not None:
+        node.prefix = prefix
+
+    attributes = body[3]["attributes"]
     if attributes is not None:
         for attribute in attributes:
             node.add_attribute(attribute, attributes[attribute])
 
-    content = body[2]["content"]
+    extras = body[4]["extras"]
+    if extras is not None:
+        for extra in extras:
+            node.add_extras(extra, extras[extra])
+
+    content = body[5]["content"]
     if content is not None:
         node.content = content
 
-    children = body[3]["children"]
+    tail = body[6]["tail"]
+    if tail is not None:
+        node.tail = tail
+
+    children = body[7]["children"]
     for child in children:
         child_node = _from_dict(child, node)
         node.add_child(child_node)
 
+    return node
+
+
+def _nsp_unique(child_nsmap: dict, parent_nsmap: dict) -> dict:
+    nsmap = dict()
+    for child_nsp in child_nsmap:
+        if child_nsp in parent_nsmap:
+            if child_nsmap[child_nsp] != parent_nsmap[child_nsp]:
+                nsmap[child_nsp] = child_nsmap[child_nsp]
+        else:
+            nsmap[child_nsp] = child_nsmap[child_nsp]
+    return nsmap
+
+
+def _process_element(e, clean) -> Node:
+    """
+    Process an lxml etree element into a Metapype node. If the clean attribute is true, then
+    remove leading and trailing whitespace from the element content.
+
+    Args:
+        e: lxml etree element
+        clean: boolean to clean leading and trailing whitespace
+
+    Returns: Node
+
+    """
+    tag = e.tag[e.tag.find("}") + 1:]  # Remove any prepended namespace
+
+    node = Node(tag)
+    node.nsmap = e.nsmap
+    node.prefix = e.prefix
+
+    if clean:
+        node.content = None if e.text.strip() == '' else e.text.strip()
+    else:
+        node.content = e.text
+
+    for name, value in e.attrib.items():
+        if "{" not in name:
+            node.add_attribute(name, value)
+    for _ in e:
+        if _.tag is not etree.Comment:
+            node.add_child(_process_element(_, clean))
+    for child in node.children:
+        child.parent = node
+        if child.nsmap == node.nsmap:
+            child.nsmap = node.nsmap  # Map to single instance of nsmap
     return node
 
 
@@ -72,8 +138,12 @@ def _serialize(node: Node) -> dict:
     """
     j = {node.name: []}
     j[node.name].append({"id": node.id})
+    j[node.name].append({"nsmap": node.nsmap})
+    j[node.name].append({"prefix": node.prefix})
     j[node.name].append({"attributes": node.attributes})
+    j[node.name].append({"extras": node.extras})
     j[node.name].append({"content": node.content})
+    j[node.name].append({"tail": node.tail})
     children = []
     for child in node.children:
         children.append(_serialize(child))
@@ -123,40 +193,61 @@ def graph(node: Node, level: int = 0) -> str:
         str: String representation of the model instance.
     """
     indent = "  " * level
-    n = f"{node.name}[{node.id}]" if node.prefix is None else f"{node.prefix}:{node.name}[{node.id}]"
+    g = f"{node.name}[{node.id}]" if node.prefix is None else f"{node.prefix}:{node.name}[{node.id}]"
     if node.content is not None:
-        n += f": {node.content}"
+        g += f": {node.content}"
     if len(node.attributes) > 0:
-        n += f" {str(node.attributes)}"
+        g += f" {str(node.attributes)}"
     if level == 0:
-        n += "\n"
+        g += "\n"
     else:
-        n = indent + "\u2570\u2500 " + n + "\n"
+        g = indent + "\u2570\u2500 " + g + "\n"
     for child in node.children:
-        n += graph(child, level + 1)
-    return n
+        g += graph(child, level + 1)
+    return g
 
 
 def from_xml(xml: str, clean: bool = True, ) -> Node:
-    _ = xml.encode("utf-8")
-    root = etree.fromstring(_)
-    return _process_element(root, clean)
+    """
+    Convert an XML model into a Metapype model. If clean is true, remove leading and trailing whitespace
+    from the element content.
+
+    Args:
+        xml: XML string to be converted
+        clean: boolean to clean leading and trailing whitespace
+
+    Returns: the root Node of the Metapype model
+
+    """
+    root = _process_element(etree.fromstring(xml.encode("utf-8")), clean)
+    return root
 
 
-def _process_element(e, clean) -> Node:
-    tag = e.tag[e.tag.find("}") + 1:]  # Remove any prepended namespace
-    node = Node(tag)
-    if clean:
-        node.content = None if e.text.strip() == '' else e.text.strip()
+def to_xml(node: Node, parent: Node = None, level: int = 0) -> str:
+    xml = ""
+    indent = "  " * level
+
+    tag = f"{node.name}" if node.prefix is None else f"{node.prefix}:{node.name}"
+
+    attributes = ""
+    if len(node.attributes) > 0:
+        attributes += " " + " ".join([f"{k}=\"{v}\"" for k, v in node.attributes.items()])
+    if parent is None:
+        attributes += " " + " ".join([f"xmlns:{k}=\"{v}\"" for k, v in node.nsmap.items()])
+    elif node.nsmap != parent.nsmap:
+        nsmap = _nsp_unique(node.nsmap, parent.nsmap)
+        attributes += " " + " ".join([f"xmlns:{k}=\"{v}\"" for k, v in nsmap.items()])
+
+    if node.content is not None:
+        content = escape(node.content)
+        open_tag = f"{indent}<{tag}{attributes}>{content}"
+        close_tag = f"</{tag}>\n"
     else:
-        node.content = e.text
-    for name, value in e.attrib.items():
-        if "{" not in name:
-            node.add_attribute(name, value)
-    node.nspmap = e.nsmap
-    node.prefix = e.prefix
-    for _ in e:
-        node.add_child(_process_element(_, clean))
+        open_tag = f"{indent}<{tag}{attributes}>\n"
+        close_tag = f"{indent}</{tag}>\n"
+
+    xml += open_tag
     for child in node.children:
-        child.parent = node
-    return node
+        xml += to_xml(child, node, level + 1)
+    xml += close_tag
+    return xml
