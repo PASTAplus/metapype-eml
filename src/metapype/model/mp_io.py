ba62ca4b986536d1926058b4b36ea8d33e068189
jide <jride@wisc.edu>
2021-02-17 17:11:36 -0600

    Removed change to use NumpyEncoder when writing JSON. It could harm other usages of metapype.

--- src/metapype/model/mp_io.py
+++ src/metapype/model/mp_io.py
@@ -15,8 +15,6 @@
     6/15/18
 """
 import json
-import numpy as np
-import pandas as pd
 
 import daiquiri
 import xml.etree.ElementTree as ET
@@ -27,37 +25,6 @@ from metapype.model.node import Node
 logger = daiquiri.getLogger("model_io: " + __name__)
 
 space = "    "
-
-
-class NumpyEncoder(json.JSONEncoder):
-    """ Custom encoder for numpy data types """
-
-    def default(self, obj):
-        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
-                            np.int16, np.int32, np.int64, np.uint8,
-                            np.uint16, np.uint32, np.uint64)):
-
-            return int(obj)
-
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
-            return float(obj)
-
-        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
-            return {'real': obj.real, 'imag': obj.imag}
-
-        elif isinstance(obj, (np.ndarray,)):
-            return obj.tolist()
-
-        elif isinstance(obj, (np.bool_)):
-            return bool(obj)
-
-        elif isinstance(obj, (np.void)):
-            return None
-
-        elif isinstance(obj, pd._libs.missing.NAType):
-            return 'NA'
-
-        return json.JSONEncoder.default(self, obj)
 
 
 def from_json(json_node: dict, parent: Node = None) -> Node:
@@ -160,7 +127,7 @@ def to_json(node: Node):
 
     """
     j = objectify(node)
-    return json.dumps(j, indent=2, cls=NumpyEncoder)
+    return json.dumps(j, indent=2)
 
 
 def from_xml_element(xml_elem, metapype_node, metapype_parent):
